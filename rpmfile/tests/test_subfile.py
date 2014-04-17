89838ed4690d6246dcad6634687b2019ffe6abc8
srossross <srossross@gmail.com>
2014-04-17 11:40:14 -0700

    FIX: tests

--- rpmfile/tests/test_subfile.py
+++ rpmfile/tests/test_subfile.py
@@ -11,42 +11,42 @@ class Test(unittest.TestCase):
 
 
     def test_seek(self):
-        
-        fd = io.BytesIO('Hello world')
+
+        fd = io.BytesIO(b'Hello world')
         sub = rpmfile._SubFile(fd, start=2, size=4)
-        
+
         sub.seek(0)
         self.assertEqual(sub.tell(), 0)
-        
+
         sub.seek(1)
         self.assertEqual(sub.tell(), 1)
-        
+
         sub.seek(1, 1)
         self.assertEqual(sub.tell(), 2)
-        
+
         sub.seek(-1, 1)
         self.assertEqual(sub.tell(), 1)
-        
+
         sub.seek(-10, 1)
         self.assertEqual(sub.tell(), 0)
-        
+
     def test_read(self):
-        
-        fd = io.BytesIO('Hello world')
+
+        fd = io.BytesIO(b'Hello world')
         sub = rpmfile._SubFile(fd, start=2, size=4)
-        
-        self.assertEqual(sub.read(), 'llo ')
-        self.assertEqual(sub.read(), '')
-        
+
+        self.assertEqual(sub.read(), b'llo ')
+        self.assertEqual(sub.read(), b'')
+
         sub.seek(0)
-        self.assertEqual(sub.read(2), 'll')
-        
+        self.assertEqual(sub.read(2), b'll')
+
         sub.seek(0)
-        self.assertEqual(sub.read(10), 'llo ')
-        
-    
+        self.assertEqual(sub.read(10), b'llo ')
+
+
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testSeek']
+    # import sys;sys.argv = ['', 'Test.testSeek']
     unittest.main()
