cd7aaedb78adb5924e93b1ed99406bc654d4de06
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2016-12-20 23:33:44 +0100

    Simplified check of compression type

--- rpmfile/__init__.py
+++ rpmfile/__init__.py
@@ -157,26 +157,13 @@ class RPMFile(object):
     def data_file(self):
         """Return the uncompressed raw CPIO data of the RPM archive."""
 
-        GZIP_MAGIC = b'\x1F\x8B'
-        XZ_MAGIC = b'\xFD\x37\x7A\x58\x5A\x00'
-
         if self._data_file is None:
             fileobj = _SubFile(self._fileobj, self.data_offset)
 
-            # check if GZip file
-            magic = fileobj.read(len(GZIP_MAGIC))
-            if GZIP_MAGIC == magic:
-                fileobj.seek(0)
-                self._data_file = gzip.GzipFile(fileobj=fileobj)
-                return self._data_file
-
-            # check if LZMA (XZ) file
-            fileobj.seek(0)
-            magic = fileobj.read(len(XZ_MAGIC))
-            if XZ_MAGIC == magic:
-                fileobj.seek(0)
+            if self.headers["archive_compression"] == b"xz":
                 self._data_file = lzma.LZMAFile(fileobj)
-                return self._data_file
+            else:
+                self._data_file = gzip.GzipFile(fileobj=fileobj)
 
         return self._data_file
 
