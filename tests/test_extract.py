2573618c422656855106e1e0ca98324a539c5c81
John Andersen <john.s.andersen@intel.com>
2019-04-25 16:03:30 -0700

    tests: Add lzma extraction test
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

---
+++ tests/test_extract.py
@@ -0,0 +1,54 @@
+import os
+import shutil
+import hashlib
+import tempfile
+import unittest
+from functools import wraps
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
+
+import rpmfile
+
+def download(url, rpmname):
+    def _downloader(func):
+        @wraps(func)
+        def wrapper(*args, **kwds):
+            rpmpath = os.path.join(args[0].tempdir, rpmname)
+            with urlopen(url) as download, \
+                    open(rpmpath, 'wb') as target_file:
+                target_file.write(download.read())
+            return func(*args, rpmpath, **kwds)
+        return wrapper
+    return _downloader
+
+
+class TempDirTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.prevdir = os.getcwd()
+        cls.tempdir = tempfile.mkdtemp()
+        os.chdir(cls.tempdir)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.tempdir)
+        os.chdir(cls.prevdir)
+
+    @download('https://download.clearlinux.org/releases/10540/clear/x86_64/os/Packages/sudo-setuid-1.8.17p1-34.x86_64.rpm', 'sudo.rpm')
+    def test_lzma_sudo(self, rpmpath):
+        with rpmfile.open(rpmpath) as rpm:
+
+            # Inspect the RPM headers
+            self.assertIn('name', rpm.headers.keys())
+            self.assertEqual(rpm.headers.get('arch', 'noarch'), b'x86_64')
+
+            members = list(rpm.getmembers())
+            self.assertEqual(len(members), 1)
+
+            fd = rpm.extractfile('./usr/bin/sudo')
+
+            calculated = hashlib.md5(fd.read()).hexdigest()
+            self.assertEqual(calculated, 'a208f3d9170ecfa69a0f4ccc78d2f8f6')
