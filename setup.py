35a4cafd078286603b2e4011c31be20564896d86
John Andersen <johnandersenpdx@gmail.com>
2020-04-01 22:08:48 -0700

    version: Bump to 1.0.1
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 setup(
     name='rpmfile',
     description='Read rpm archive files',
-    version="0.1.6",
+    version="1.0.1",
     author='Sean Ross-Ross',
     author_email='srossross@gmail.com',
     url='https://github.com/srossross/rpmfile',
