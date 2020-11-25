88f875066b5422590e8aecb9863f80f668d68667
John Andersen <johnandersenpdx@gmail.com>
2020-11-25 10:25:17 -0800

    release: Version 1.0.6
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 setup(
     name="rpmfile",
     description="Read rpm archive files",
-    version="1.0.5",
+    version="1.0.6",
     author="Sean Ross-Ross",
     author_email="srossross@gmail.com",
     url="https://github.com/srossross/rpmfile",
