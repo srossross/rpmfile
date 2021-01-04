2773f95e2f052b8e2a1ebaa7da27fabe42b58392
KOLANICH <KOLANICH@users.noreply.github.com>
2021-01-04 20:47:39 +0000

    setup: Moved the metadata into setup.cfg
    Fetched the version using setuptools_scm

--- setup.py
+++ setup.py
@@ -2,41 +2,7 @@
 @author: sean
 """
 
-from setuptools import setup, find_packages
-from os.path import isfile
+from setuptools import setup
 
-if isfile("README.md"):
-    with open("README.md") as readme:
-        long_description = readme.read()
-else:
-    long_description = "???"
-
-setup(
-    name="rpmfile",
-    description="Read rpm archive files",
-    version="1.0.6",
-    author="Sean Ross-Ross",
-    author_email="srossross@gmail.com",
-    url="https://github.com/srossross/rpmfile",
-    license="MIT",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
-    tests_require=["zstandard>=0.13.0"],
-    classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ],
-    entry_points={
-        "console_scripts": ["rpmfile = rpmfile.cli:console_script_entry_point"],
-    },
-)
+if __name__ == "__main__":
+    setup()
